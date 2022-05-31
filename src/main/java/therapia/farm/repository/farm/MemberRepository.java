25a14fe2e553c2daf00eefb77f483b42b61985a6
Jiyeon Jeong <jung_j_yeon@naver.com>
2022-06-01 01:20:33 +0900

    Feat : MemberService Test 추가

--- src/main/java/therapia/farm/repository/farm/MemberRepository.java
+++ src/main/java/therapia/farm/repository/farm/MemberRepository.java
@@ -1,5 +1,4 @@
 package therapia.farm.repository.farm;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import therapia.farm.domain.farm.Member;
